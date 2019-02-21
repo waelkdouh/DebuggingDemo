2a932c8623d5c6b5c5e08d54fb58e97e8d0b0440
Wael Kdouh <waelkdouh@gmail.com>
2019-02-21 14:17:36 -0500

    Initial commit

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person()
+            {
+                Age = 42,
+                FirstName = "wael",
+                LastName = "kdouh"
+            };
+
+            person.LastName = "new Name";
+
+            int i = 10;
+            i++;
+            i++;
+            i++;
+            Foo();
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            i++;
+            Foo();
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //string o1 = "test";
+            //string o2 = o1;
+            //string o3 = o2;
+
+            //int i = FooBar();
+            //try
+            //{
+            //    throw new ArgumentException();
+            //}
+            //catch (Exception)
+            //{
+            //}
+
+            //Console.ReadLine();
+            //Console.WriteLine("Hello");
+
+        }
+
+        private static int FooBar()
+        {
+            return 100;
+        }
+
+        private static void Foo()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+
+            }
+            Bar();
+        }
+
+        private static void Bar()
+        {
+            string s = "!";
+            int z = 42;
+        }
+
+        private static int Add(int num1, int num2)
+        {
+            return num1 +num2;
+        }
+
+    }
+}
